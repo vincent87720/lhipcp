87a6648b5379e29c7bcfaecb389b6b348775efd0
vincent87720 <vincent87720@yahoo.com.tw>
2021-11-11 22:56:07 +0800

    Add: pwa;

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  pwa: {
+    name: "勞健保計算",
+    themeColor: '#607d8b',
+    msTileColor: '#607d8b',
+    manifestOptions: {
+      start_url: '.',
+      background_color: '#4c89fe'
+    },
+    workboxPluginMode: 'GenerateSW',
+    workboxOptions: {
+    }
+  },
   "transpileDependencies": [
     "vuetify"
   ],
